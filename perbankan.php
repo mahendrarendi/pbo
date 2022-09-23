f7c66a2786bd5c624300f05b6f42f7a9fba2fb91
Rendi Nicolas Mahendra <68139718+mahendrarendi@users.noreply.github.com>
2022-09-23 19:13:54 +0700

    program perbankan

--- perbankan.php
+++ perbankan.php
@@ -1,27 +1,37 @@
+// gunakan tag html sebagai bahasa markup
 <!DOCTYPE html>
 <html>
 <head>
     
 </head>
 <body>
+    
+    // tambahkan tag div untuk pengelompokan dan buat class program
     <div class="container">
+
+        // tambahkan tag php untuk menjalankan program php
         <?php
+            // buat class program php dengan menyimpan variabel saldo yg menjadi parent
             class Tabungan  {
                 protected $saldo;
             }
+            // buat class diagram yg menjadi child
             class PengambilanUang extends Tabungan  {
                 private $proteksi;
 
+                // membuat fungsi yg di simpan pada variabel saldo
                 public function __construct($saldo)
                 {
                     $this->saldo=$saldo;
                     $this->proteksi = 1000;
                 }
-
+                
+                // memanggil fungsi saldo dan memberikan pesan
                 public function getSaldo(){
                     return "Uang yang ditabung : " . $this->saldo . "<br> Uang yang diproteksi : " . $this->proteksi;
                 } 
 
+                // membuat fungsi untuk melakukan kalkulasi jumlah
                 public function ambilUang($jumlah){
                     if($jumlah == 4500){
                         return "Uang yang akan diambil : " . $jumlah . " false <br> Saldo sekarang : " . $this->saldo;
@@ -31,13 +41,14 @@
                 }
             }
 
+            // menampilkan hasil kalkulasi hasil dari tabungan yg tersisa
             $tabungan = new PengambilanUang(5000);
             echo $tabungan->getSaldo();
             echo "<br> ----------------------------------------- <br>";
             echo $tabungan->ambilUang(4500);
             echo "<br> ----------------------------------------- <br>";
             echo $tabungan->ambilUang(2500);
-            echo "develop @mahebdrarendi";
+            echo "develop @mahendrarendi";
         ?>
     </div>
 
